7816ae0829ec40346530daca937486038f533c69
kimchieater <jasonj19276@gmail.com>
2024-02-13 11:31:34 +1100

    created a new project

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,14 +1,15 @@
+'use client'
+
+
+import {useState} from "react";
+
 
 export default function List(){
 
   let products = ["tomatoes", 'pasta', "coconut"];
-
+  let [likes, setLikes] = useState([0,0,0]);
 
   return(
-
-
-  
-
     <div>
       <h4 className="title">List of Items</h4>
       
@@ -16,10 +17,22 @@ export default function List(){
       products.map((a, i)=>{
         return(
           <div>
-            <h4 className="food" key={i}>
+            <div className="food" key={i}>
             <img src={`/food${i}.png`} className="food-img"></img>
               {a}
-            </h4>
+              <span>{likes[i]}</span>
+            <button onClick={()=>{
+              let copy = [...likes];
+              copy[i] += 1;
+              setLikes(copy);
+            }}>+</button>
+            <button onClick={()=>{
+              {
+                likes > 0 ? setLikes(minusLikes => minusLikes - 1) : null;
+              }
+            
+            }}>-</button>
+            </div>                    
           </div>
         )
       })
